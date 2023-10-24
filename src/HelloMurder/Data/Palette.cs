15791cf386655a127866f7d6c4dddf4fd9e8fa18
Leviathan <alexmbelland@gmail.com>
2023-10-24 03:17:32 -0400

    palette

--- src/HelloMurder/Data/Palette.cs
+++ src/HelloMurder/Data/Palette.cs
@@ -6,16 +6,16 @@ namespace HelloMurder.Core
     {
         public readonly static Color[] Colors = new Color[]
         {
-            Color.FromHex("000000"),
-            Color.FromHex("1B222A"),
+            Color.FromHex("000000"), //0
+            Color.FromHex("000000"),//1
             Color.FromHex("2D334F"),
             Color.FromHex("847E87"),
             Color.FromHex("FB43C5"),
             Color.FromHex("CBDBFC"),
-            Color.FromHex("737480"),
-            Color.FromHex("737480"),
+            Color.FromHex("737480"), 
+            Color.FromHex("000000"),//7
             Color.FromHex("FFB3AB"),
-            Color.FromHex("51AD26")
+            Color.FromHex("fff200") //9
         };
 
         static Palette()
